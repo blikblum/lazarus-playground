blikblum/lazarus-playground

commit 1ffd67d45560bf143e9e51ddd5f4fd3cb5db7eed

Debugger: keep register-names in register window, while debugger is running (show <unavailable>). This way scroll pos is preserved. Issue 0032654
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@60049 4005530d-fff6-0310-9dd1-cebe43e6787f

debugger/registersdlg.pp
@@ -396,8 +396,14 @@ begin
   FNeedUpdateAgain := False;
 
   Reg := GetCurrentRegisters;
-  if Reg = nil then begin
-    lvRegisters.Items.Clear;
+  if (Reg = nil) or (reg.DataValidity<> ddsValid) then begin
+    if (DebugBoss.Debugger = nil) or not (DebugBoss.Debugger.State in [dsPause, dsInternalPause, dsRun]) then
+      lvRegisters.Items.Clear;
+
+    if (reg <> nil) then
+      reg.Count;
+    for n := 0 to lvRegisters.Items.Count - 1 do
+      lvRegisters.Items[n].SubItems[0] := '<Unavailable>';
     exit;
   end;
 

commit 5901bd777f624af6f3307da1581b6b15c16a6bff

Debugger: Register Window, fix updating register values. Broken in some cases since r60049
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@60342 4005530d-fff6-0310-9dd1-cebe43e6787f

debugger/registersdlg.pp
@@ -395,21 +395,21 @@ begin
   end;
   FNeedUpdateAgain := False;
 
-  Reg := GetCurrentRegisters;
-  if (Reg = nil) or (reg.DataValidity<> ddsValid) then begin
-    if (DebugBoss = nil) or not (DebugBoss.State in [dsPause, dsInternalPause, dsRun]) then
-      lvRegisters.Items.Clear;
-
-    if (reg <> nil) then
-      reg.Count;
-    for n := 0 to lvRegisters.Items.Count - 1 do
-      lvRegisters.Items[n].SubItems[0] := '<Unavailable>';
-    exit;
-  end;
-
-  List := TStringList.Create;
+  BeginUpdate;
   try
-    BeginUpdate;
+    Reg := GetCurrentRegisters;
+    if (Reg = nil) or (reg.DataValidity<> ddsValid) then begin
+      if (DebugBoss = nil) or not (DebugBoss.State in [dsPause, dsInternalPause, dsRun]) then
+        lvRegisters.Items.Clear;
+
+      if (reg <> nil) then
+        reg.Count;
+      for n := 0 to lvRegisters.Items.Count - 1 do
+        lvRegisters.Items[n].SubItems[0] := '<Unavailable>';
+      exit;
+    end;
+
+    List := TStringList.Create;
     try
       //Get existing items
       for n := 0 to lvRegisters.Items.Count - 1 do
@@ -450,10 +450,10 @@ begin
         lvRegisters.Items.Delete(TListItem(List.Objects[n]).Index);
 
     finally
-      EndUpdate;
+      List.Free;
     end;
   finally
-    List.Free;
+    EndUpdate;
   end;
 
   lvRegistersSelectItem(nil, nil, True);